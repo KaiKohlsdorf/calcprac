809dfc0bee67b2897db70c94f1f2945afd2919aa
Kai Kohlsdorf <kai.w.kohlsdorf@gmail.com>
2022-06-12 19:26:35 -0700

    Add initial function and variables, initial html, bootstrap, jquery, and README.md.

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+const number1 = parstInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+
+alert(add(number1, number2));
